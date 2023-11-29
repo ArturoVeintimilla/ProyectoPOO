c7c6df14ae4a3b66743e7f0320e258cef2eab89f
ArturoVeintimilla <asveinti@espol.edu.ec>
2023-11-29 16:08:27 -0500

    Creacion de la clase Utilitaria

---
+++ src/main/java/ec/edu/espol/proyectopoo/Utilitaria.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Utilitaria {
+    public static ArrayList<Ficha>crearManoJugador(){
+        ArrayList<Ficha>mano=new ArrayList();
+        while(mano.size()<=4){
+            int numeroAleatorio1 = (int) (Math.random() * 6) + 1;
+            int numeroAleatorio2= (int) (Math.random() * 6) + 1;
+            Ficha f=new Ficha(numeroAleatorio1,numeroAleatorio2);
+            mano.add(f);
+        }
+        Ficha fc=new FichaComodin();
+        mano.add(fc);
+        return mano;
+    }
+}
