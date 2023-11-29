538e03166c5ee335a6f225f9444b23422b2dec86
JuanAyala619 <juaseaya@espol.edu.ec>
2023-11-29 16:08:08 -0500

    Constructor y getters

--- src/main/java/ec/edu/espol/proyectopoo/Jugador.java
+++ src/main/java/ec/edu/espol/proyectopoo/Jugador.java
@@ -4,10 +4,31 @@
  */
 package ec.edu.espol.proyectopoo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author TOSHIBA
  */
 public class Jugador {
     private String nombre;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String n, ArrayList<Ficha> mano) {
+        this.nombre = n;
+        this.mano = mano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public Ficha getFicha(int i){
+        if (i>0 && i<=mano.size())
+            return mano.get(i-1);
+        else
+            return null;
+    }
+    
+    
 }
