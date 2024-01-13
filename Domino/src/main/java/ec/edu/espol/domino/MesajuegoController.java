423f450a56edc34f5b38127293a67d0135bb9e82
Levin514 <levezmor@espol.edu.ec>
2024-01-13 18:28:37 -0500

    Interfaz Beta

--- Domino/src/main/java/ec/edu/espol/domino/MesajuegoController.java
+++ Domino/src/main/java/ec/edu/espol/domino/MesajuegoController.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -30,10 +33,13 @@ public class MesajuegoController implements Initializable {
     private HBox manojugador;
     @FXML
     private HBox mesafichas;
+
     @FXML
-    private VBox ficha_individual_m;
+    private ScrollPane idScrollPane;
     @FXML
-    private VBox ficha_individual;
+    private Button bttInicio;
+    @FXML
+    private Button bttFin;
 
     /**
      * Initializes the controller class.
@@ -41,12 +47,19 @@ public class MesajuegoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
         ImageView im_jL1;
         ImageView im_jL2;
         ImageView im_mL1;
         ImageView im_mL2;
-        for(Ficha f: mano_m){
-            ficha_individual_m= new VBox();
+        
+        Juego game = new Juego();
+        JuegoNuevo.NuevoJuego(game);
+
+        for(Ficha f: game.getJugadores().get(0).getMano()){
+            
+            HBox ficha_individual_m = new HBox();
+            
             if(!(f instanceof FichaComodin)){
                 int lado1= f.getLado1();
                 int lado2= f.getLado2();
@@ -59,17 +72,28 @@ public class MesajuegoController implements Initializable {
                 if(lado2== 4)
                     im_jL2= new ImageView("dado4.jpg");
                 else
-                    im_jL2= new ImageView("dado"+l_2+"png");
+                    im_jL2= new ImageView("dado"+l_2+".png");
             }
             else{
                 im_jL1= new ImageView("comodin.jpg");
                 im_jL2= new ImageView("comodin.jpg");
             }
+            im_jL1.setFitHeight(80);
+            im_jL1.setFitWidth(80);
+            im_jL2.setFitWidth(80);
+            im_jL2.setFitHeight(80);
+            
             ficha_individual_m.getChildren().addAll(im_jL1,im_jL2);
             manomaquina.getChildren().add(ficha_individual_m);
+            manomaquina.setSpacing(10);
+            manomaquina.setAlignment(Pos.CENTER);
+            
         }
-        for(Ficha f: mano_j){
-            ficha_individual= new VBox();
+        
+        for(Ficha f: game.getJugadores().get(1).getMano()){
+            
+            HBox ficha_individual = new HBox();
+            
             if(!(f instanceof FichaComodin)){
                 int lado1= f.getLado1();
                 int lado2= f.getLado2();
@@ -82,22 +106,27 @@ public class MesajuegoController implements Initializable {
                 if(lado2== 4)
                     im_mL2= new ImageView("dado4.jpg");
                 else
-                    im_mL2= new ImageView("dado"+l_2+"png");
+                    im_mL2= new ImageView("dado"+l_2+".png");
             }
             else{
                 im_mL1= new ImageView("comodin.jpg");
                 im_mL2= new ImageView("comodin.jpg");
             }
-            ficha_individual= new VBox();
+
+            im_mL1.setFitHeight(80);
+            im_mL1.setFitWidth(80);
+            im_mL2.setFitWidth(80);
+            im_mL2.setFitHeight(80);
+            
             ficha_individual.getChildren().addAll(im_mL1,im_mL2);
             manojugador.getChildren().add(ficha_individual);
+            manojugador.setSpacing(10);
+            manojugador.setAlignment(Pos.CENTER);
         }
+        
+        
     }    
 
-    @FXML
-    private void ficha_elegida(MouseEvent event) {
-        
-    }
 
     
 }
--- DominoPrimerParcial/src/main/java/ec/edu/espol/proyectopoo/ProyectoPOO.java
+++ DominoPrimerParcial/src/main/java/ec/edu/espol/proyectopoo/ProyectoPOO.java
@@ -176,3 +176,6 @@ public class ProyectoPOO {
                 System.out.println("El ganador es: " + game.getJugadores().get(1).getNombre());
                 
         }
+
+    }
+}
