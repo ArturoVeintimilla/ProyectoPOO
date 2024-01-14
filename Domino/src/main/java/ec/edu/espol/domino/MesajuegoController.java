874b251d4bde7ca68f341e71e716f3f154158465
JuanAyala619 <juaseaya@espol.edu.ec>
2024-01-13 21:40:36 -0500

    eventos botones

--- Domino/src/main/java/ec/edu/espol/domino/MesajuegoController.java
+++ Domino/src/main/java/ec/edu/espol/domino/MesajuegoController.java
@@ -48,84 +48,53 @@ public class MesajuegoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         
-        ImageView im_jL1;
-        ImageView im_jL2;
-        ImageView im_mL1;
-        ImageView im_mL2;
-        
         Juego game = new Juego();
-        JuegoNuevo.NuevoJuego(game);
-
-        for(Ficha f: game.getJugadores().get(0).getMano()){
-            
-            HBox ficha_individual_m = new HBox();
-            
+        JuegoNuevo.NuevoJuego(game);   
+    } 
+    private void actualizar_fichas(ArrayList<Ficha> game,HBox lugar){
+        ImageView im_L1;
+        ImageView im_L2;
+        for(Ficha f: game){
+            HBox ficha_individual_m= new HBox();
             if(!(f instanceof FichaComodin)){
                 int lado1= f.getLado1();
                 int lado2= f.getLado2();
                 String l_1=""+lado1;
                 String l_2=""+lado2;
                 if(lado1 == 4)
-                    im_jL1= new ImageView("dado4.jpg");
+                    im_L1= new ImageView("dado4.jpg");
                 else
-                    im_jL1= new ImageView("dado"+l_1+".png");
+                    im_L1= new ImageView("dado"+l_1+".png");
                 if(lado2== 4)
-                    im_jL2= new ImageView("dado4.jpg");
+                    im_L2= new ImageView("dado4.jpg");
                 else
-                    im_jL2= new ImageView("dado"+l_2+".png");
+                    im_L2= new ImageView("dado"+l_2+".png");
             }
             else{
-                im_jL1= new ImageView("comodin.jpg");
-                im_jL2= new ImageView("comodin.jpg");
+                im_L1= new ImageView("comodin.jpg");
+                im_L2= new ImageView("comodin.jpg");
             }
-            im_jL1.setFitHeight(80);
-            im_jL1.setFitWidth(80);
-            im_jL2.setFitWidth(80);
-            im_jL2.setFitHeight(80);
-            
-            ficha_individual_m.getChildren().addAll(im_jL1,im_jL2);
-            manomaquina.getChildren().add(ficha_individual_m);
-            manomaquina.setSpacing(10);
-            manomaquina.setAlignment(Pos.CENTER);
-            
+            im_L1.setFitHeight(80);
+            im_L1.setFitWidth(80);
+            im_L2.setFitHeight(80);
+            im_L2.setFitWidth(80);
+            ficha_individual_m.getChildren().addAll(im_L1,im_L2);
+            lugar.getChildren().add(ficha_individual_m);
+            lugar.setSpacing(10);
+            lugar.setAlignment(Pos.CENTER);
+            ficha_individual_m.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent t) -> {
+                
+            }
         }
-        
-        for(Ficha f: game.getJugadores().get(1).getMano()){
-            
-            HBox ficha_individual = new HBox();
-            
-            if(!(f instanceof FichaComodin)){
-                int lado1= f.getLado1();
-                int lado2= f.getLado2();
-                String l_1= ""+lado1;
-                String l_2=""+lado2;
-                if(lado1 == 4)
-                    im_mL1= new ImageView("dado4.jpg");
-                else
-                    im_mL1= new ImageView("dado"+l_1+".png");
-                if(lado2== 4)
-                    im_mL2= new ImageView("dado4.jpg");
-                else
-                    im_mL2= new ImageView("dado"+l_2+".png");
-            }
-            else{
-                im_mL1= new ImageView("comodin.jpg");
-                im_mL2= new ImageView("comodin.jpg");
-            }
-
-            im_mL1.setFitHeight(80);
-            im_mL1.setFitWidth(80);
-            im_mL2.setFitWidth(80);
-            im_mL2.setFitHeight(80);
-            
-            ficha_individual.getChildren().addAll(im_mL1,im_mL2);
-            manojugador.getChildren().add(ficha_individual);
-            manojugador.setSpacing(10);
-            manojugador.setAlignment(Pos.CENTER);
-        }
-        
-        
     }    
+
+    @FXML
+    private void ficha_al_inicio(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha_al_final(MouseEvent event) {
+    }
 
 
     
